1b69e9dfbb6edda9c347b3be2025fa9a9c926e5e
Tony <Tonymallios1@gmail.com>
2024-03-03 18:09:45 -0600

    3/3/24

--- Hello World/Assets/Week 6/Scripts/PlayerController.cs
+++ Hello World/Assets/Week 6/Scripts/PlayerController.cs
@@ -14,12 +14,33 @@ namespace Week6
 
         Rigidbody rb;
 
-        [SerializeField] float speedStrength = 5f;
-        [SerializeField] float jumpStrength = 5f;
+        [SerializeField] float speedStrength = 8;
+        [SerializeField] float tiredSpeedStrength = 3;
+        [SerializeField] float jumpStrength = 3;
+
+        [SerializeField] float stamina = 100f;
+        [SerializeField] float maxStamina = 100f;
+
+        [SerializeField] float runCost = 1f;
+
+        [SerializeField] float sensX = 200f;
+        [SerializeField] float sensY = 200f;
+
+        [SerializeField] Transform orientation;
+
+        float xRot;
+        float yRot;
+
+        float timer = 0.0f;
+        float interlopeTime = 0.1f;
+        float regainingStaminaInterlopeTime = 0.2f;
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody>();
+
+            //Cursor.lockState = CursorLockMode.Locked;
+            //Cursor.visible = false;
 
             mappings = new PlayerControllerMappings();
 
@@ -53,17 +74,63 @@ namespace Week6
         void Update()
         {
             Vector2 input = moveAction.ReadValue<Vector2>();
-            input *= speedStrength;
+            
+            //refilling stamina 
+            if ((input.x == 0 || input.y == 0) && stamina < maxStamina)
+            {
+                timer += Time.deltaTime;
 
+                if (timer >= regainingStaminaInterlopeTime)
+                {
+                    timer = 0.0f;
+
+                    stamina++;
+                }
+                
+            }
+            //using stamina
+            if (input.x != 0 || input.y != 0)
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= interlopeTime)
+                {
+                    timer = 0.0f;
+
+                    stamina -= runCost;
+                }
+
+                if (stamina < 0) stamina = 0;
+            }
+            float speed = speedStrength;
+            //Setting tired speed if stamina is 0
+            if (stamina == 0)
+            {
+                speed = tiredSpeedStrength;
+            }
+            else speed = speedStrength;
+
+            input *= speed;
             rb.velocity = new Vector3(input.x, rb.velocity.y, input.y);
-            //transform.rotation *= Quaternion.Euler(0, transform.rotation.y + input.x, 0);
+            //rb.rotation = Quaternion.AngleAxis(rb.rotation, rb.velocity);
+
+            //Camera controls
+            /*float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+            yRot += mouseX;
+            xRot -= mouseY;
+            xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+            transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+            orientation.rotation = Quaternion.Euler(0, yRot, 0);*/
         }
 
         void Jump(InputAction.CallbackContext context)
         {
-            Debug.Log("jump wow");
-            if (!IsGrounded()) return;
-            rb.AddForce(Vector3.up * jumpStrength);
+            //Debug.Log("jumped");
+            //if (!IsGrounded()) return;
+            rb.AddForce(Vector3.up * jumpStrength * 100);
         }
 
         bool IsGrounded()
