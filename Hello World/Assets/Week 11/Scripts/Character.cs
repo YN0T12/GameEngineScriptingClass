015bd291cd2036e00909bd597dea5b21fe0580ec
Tony <Tonymallios1@gmail.com>
2024-04-15 14:25:10 -0500

    4/15/24

--- Hello World/Assets/Week 11/Scripts/Character.cs
+++ Hello World/Assets/Week 11/Scripts/Character.cs
@@ -19,6 +19,15 @@ namespace CharacterEditor
         public void Load()
         {
             //Load materials from the MaterialManager and pass in the id pulled from each PlayerPref here
+            m_ArmR.material = MaterialManager.Get(BodyTypes.Arm, PlayerPrefs.GetInt("ArmColor"));
+            m_ArmL.material = MaterialManager.Get(BodyTypes.Arm, PlayerPrefs.GetInt("ArmColor"));
+
+            m_LegR.material = MaterialManager.Get(BodyTypes.Leg, PlayerPrefs.GetInt("LegColor"));
+            m_LegL.material = MaterialManager.Get(BodyTypes.Leg, PlayerPrefs.GetInt("LegColor"));
+
+            m_Head.material = MaterialManager.Get(BodyTypes.Head, PlayerPrefs.GetInt("HeadColor"));
+
+            m_Body.material = MaterialManager.Get(BodyTypes.Body, PlayerPrefs.GetInt("BodyColor"));
         }
     }
 }
--- Hello World/Assets/Week 11/Scripts/CharacterEditor.cs
+++ Hello World/Assets/Week 11/Scripts/CharacterEditor.cs
@@ -13,14 +13,17 @@ namespace CharacterEditor
         [SerializeField] Character character;
 
         int id;
-        BodyTypes bodyType = BodyTypes.Head;
+        public BodyTypes bodyType = BodyTypes.Head;
 
         private void Awake()
         {
             //TODO: Setup some button listeners to call the NextMaterial, NextBodyPart, and LoadGame functions
+            //nextMaterial.Invoke("NextMaterial", 0);
+            //nextBodyPart.Invoke("NextBodyPart", 0);
+            //loadGame.Invoke("LoadGame", 0);
         }
 
-        void NextMaterial()
+        public void NextMaterial()
         {
             //TODO: Add 1 to the value of id and if it is 3 or more then reset back to 0
             id++;
@@ -28,29 +31,73 @@ namespace CharacterEditor
             //TODO: Make a switch case for each BodyType and save the value of id to the correct PlayerPref
             switch (bodyType)
             {
-                case BodyTypes.Arm: PlayerPrefs.SetInt("ArmColor", id);
-                case BodyTypes.Leg: PlayerPrefs.SetInt("LegColor", id);
-                case BodyTypes.Head: PlayerPrefs.SetInt("HeadColor", id);
-                case BodyTypes.Body: PlayerPrefs.SetInt("TorsoColor", id);
-                default: PlayerPrefs.SetInt("ArmColor", id);
+                case BodyTypes.Arm: 
+                    PlayerPrefs.SetInt("ArmColor", id);
+                    break;
+                case BodyTypes.Leg: 
+                    PlayerPrefs.SetInt("LegColor", id);
+                    break;
+                case BodyTypes.Head: 
+                    PlayerPrefs.SetInt("HeadColor", id);
+                    break;
+                case BodyTypes.Body: 
+                    PlayerPrefs.SetInt("BodyColor", id);
+                    break;
+                default: 
+                    PlayerPrefs.SetInt("ArmColor", id);
+                    break;
             }
             //TODO: Tell the character to load to get the updated body piece
             character.Load();
         }
 
-        void NextBodyPart()
+        public void NextBodyPart()
         {
             //TODO: Setup a switch case that will go through the different body types
             //      ie if the current type is Head and we click next then set it to Body
-
+            switch (bodyType)
+            {
+                case BodyTypes.Arm:
+                    bodyType = BodyTypes.Leg;
+                    break;
+                case BodyTypes.Leg:
+                    bodyType = BodyTypes.Head;
+                    break;
+                case BodyTypes.Head:
+                    bodyType = BodyTypes.Body;
+                    break;
+                case BodyTypes.Body:
+                    bodyType = BodyTypes.Arm;
+                    break;
+                default:
+                    bodyType = BodyTypes.Arm;
+                    break;
+            }
             //TODO: Then setup another switch case that will get the current saved value
             //      from the player prefs for the current body type and set it to id
-
+            switch (bodyType)
+            {
+                case BodyTypes.Arm:
+                    id = PlayerPrefs.GetInt("ArmColor");
+                    break;
+                case BodyTypes.Leg:
+                    id = PlayerPrefs.GetInt("LegColor");
+                    break;
+                case BodyTypes.Head:
+                    id = PlayerPrefs.GetInt("HeadColor");
+                    break;
+                case BodyTypes.Body:
+                    id = PlayerPrefs.GetInt("BodyColor");
+                    break;
+                default:
+                    id = PlayerPrefs.GetInt("ArmColor");
+                    break;
+            }
         }
 
-        void LoadGame()
+        public void LoadGame()
         {
-            SceneManager.LoadScene("Character Creator");
+            SceneManager.LoadScene("CharacterCreator");
         }
     }
 }
